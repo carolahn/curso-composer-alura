20d9eaddfa079c7dfd4da1420cf37f6a16aea952
ahncarolina <ahncarolina@gmail.com>
2021-11-03 17:08:04 -0300

    Initial commit

---
+++ buscar-cursos-com-class.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'vendor/autoload.php';
+// require 'src/Buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+Teste::metodo();
+Teste2::metodo();
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$courses = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($courses as $course) {
+    echo $course . PHP_EOL;
+}
+
+exibeMensagem("Hello, world! com quebra de linha");
